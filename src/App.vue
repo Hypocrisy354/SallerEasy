616c6b20a60dbfab71506f9dded2dfd6c335709c
Hypocrisy354 <hypocrisy354@gmail.com>
2022-06-05 16:13:32 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,60 @@
 
     <div class="box">
       <!-- 渲染 Left 组件和 Right 组件 -->
+      <!-- <img src="http://10.100.69.219:8301/auth/captcha?key=1234"/> -->
+      <button @click="getToken">getToken</button>
+      <button @click="testToken">testToken</button>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+import qs from 'qs'
+import $ from '@/assets/jquery-1.11.3.js'
+export default {
+  data() {
+    return {
+      token1: 'ZXR1cmlhbDoxMjM0NTY=',
+      token2: '',
+      prase: {
+        grant_type: 'password',
+        username: 'eturial',
+        password: '1234qwer',
+        key: '1234',
+        code: '7592'
+      }
+      // data: qs.stringify(this.prase)
+    }
+  },
+  methods: {
+    getToken() {
+      this.axios({
+        method: 'POST',
+        url: 'auth/oauth/token',
+        headers: {
+          Authorization: 'Basic ' + this.token1
+          // 'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        data: qs.stringify(this.prase)
+      })
+        .then(res => {
+          console.log(res.data.access_token);
+          this.token2 = res.data.access_token
+        })
+        .catch(err => console.log(err))
+    },
+    testToken() {
+      this.axios({
+        url: 'system/info',
+        headers: {
+          Authorization: 'Bearer ' + this.token2
+        }
+      }).then( res => {
+        console.log(res.data);
+      })
+    }
+  }
+}
 </script>
 
 <style lang="less">
--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axios from 'axios'
 
+Vue.prototype.axios = axios
 Vue.config.productionTip = false
+axios.defaults.baseURL = 'http://10.100.69.219:8301/'
 
 new Vue({
-  render: h => h(App),
+  render: h => h(App)
 }).$mount('#app')
